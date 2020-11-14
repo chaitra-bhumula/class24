a0f26915c860ab9e4be39be1245c4f890d27317d
chaitra-bhumula <71412450+chaitra-bhumula@users.noreply.github.com>
2020-11-14 18:17:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var pig1;
+var log1;
+var bird;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,300,70,70);
+    box2 = new Box(1000,300,70,70);
+    ground = new Ground(600,height,1200,20)
+    pig1 = new Pig(850,300,70,70);
+    pig2 = new Pig(850,250,70,70);
+    log1 = new Log(850,260,400,20, PI);
+    box3 = new Box(700,250,70,70);
+    box4 = new Box(1000,250,70,70);
+    log2 = new Log(850,230,400,20, PI);
+    box5 = new Box(850,210,70,70);
+    log3 = new Log(830,210,150,20, -PI/3);
+    log4 = new Log(870,210,150,20, PI/4);
+    angryBird = new Bird(300,200,70,70);
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+    box5.display();
+    log3.display();
+    log4.display();
+    angryBird.display();
+}
